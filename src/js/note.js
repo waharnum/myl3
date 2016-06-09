06c790bf1712e79adcff21f9e7bc2432f5b6a422
Alan Harnum <waharnum@gmail.com>
2016-06-09 11:57:09 -0400

    More refactoring

--- src/js/note.js
+++ src/js/note.js
@@ -26,18 +26,25 @@
     });
 
     // Base mixin grade for displaying entries
-    fluid.defaults("floe.dashboard.entry.displayed", {
+    fluid.defaults("floe.dashboard.displayedEntry", {
         gradeNames: ["floe.chartAuthoring.valueBinding"],
+        selectors: {
+            delete: ".flc-entry-delete"    
+        },
         listeners: {
             "onPouchDocDeleted.removeNoteMarkup": {
-                funcName: "floe.dashboard.entry.displayed.removeEntryMarkup",
+                funcName: "floe.dashboard.displayedEntry.removeEntryMarkup",
                 args: "{that}"
             },
             "onCreate.renderEntryTemplate": {
-                funcName: "floe.dashboard.entry.displayed.renderEntryTemplate",
+                funcName: "floe.dashboard.displayedEntry.renderEntryTemplate",
                 args: "{that}",
                 // Needs to beat any value binding
                 priority: "first"
+            },
+            "onEntryTemplateRendered.bindDelete": {
+                funcName: "floe.dashboard.displayedEntry.bindDelete",
+                args: "{that}"
             }
         },
         events: {
@@ -49,46 +56,39 @@
         // }
     });
 
-    fluid.defaults("floe.dashboard.note.displayed", {
-        gradeNames: ["floe.dashboard.note", "floe.dashboard.entry.displayed"],
-        // A key/value of selectorName: model.path
-        selectors: {
-            created: ".flc-note-created",
-            lastModified: ".flc-note-lastModified",
-            text: ".flc-note-text",
-            delete: ".flc-note-delete"
-        },
-        bindings: {
-            created: "createdDatePretty",
-            lastModified: "lastModifiedDatePretty",
-            text: "text"
-        },
-        listeners: {
-            "onEntryTemplateRendered.bindDelete": {
-                funcName: "floe.dashboard.entry.displayed.bindDelete",
-                args: "{that}"
-            }
-        },
-        resources: {
-            entryTemplate: "Created: <span class='flc-note-created'></span><br/>Last Modified: <span class='flc-note-lastModified'></span><br/><a href='#' class='flc-note-delete'>Delete Note</a><br/><textarea  class='flc-note-text' cols=50 rows=3></textarea>"
-        }
-    });
-
-    floe.dashboard.entry.displayed.removeEntryMarkup = function (that) {
+    floe.dashboard.displayedEntry.removeEntryMarkup = function (that) {
         that.container.remove();
     };
 
-    floe.dashboard.entry.displayed.renderEntryTemplate = function (that) {
+    floe.dashboard.displayedEntry.renderEntryTemplate = function (that) {
         var entryTemplate = that.options.resources.entryTemplate;
         that.container.append(entryTemplate);
         that.events.onEntryTemplateRendered.fire();
     };
 
-    floe.dashboard.entry.displayed.bindDelete = function (that) {
+    floe.dashboard.displayedEntry.bindDelete = function (that) {
         var deleteControl = that.locate("delete");
         deleteControl.click(function (e) {
             e.preventDefault();
             that.delete();
         });
     };
+
+    fluid.defaults("floe.dashboard.note.displayed", {
+        gradeNames: ["floe.dashboard.note", "floe.dashboard.displayedEntry"],
+        // A key/value of selectorName: model.path
+        selectors: {
+            created: ".flc-note-created",
+            lastModified: ".flc-note-lastModified",
+            text: ".flc-note-text"
+        },
+        bindings: {
+            created: "createdDatePretty",
+            lastModified: "lastModifiedDatePretty",
+            text: "text"
+        },
+        resources: {
+            entryTemplate: "Created: <span class='flc-note-created'></span><br/>Last Modified: <span class='flc-note-lastModified'></span><br/><a href='#' class='flc-entry-delete'>Delete Note</a><br/><textarea  class='flc-note-text' cols=50 rows=3></textarea>"
+        }
+    });
 })(jQuery, fluid);
